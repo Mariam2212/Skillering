c4c0e424c1bf43bb83436c8a428327cd0f03732f
Mariam Gad <mariamgad440@gmail.com>
2025-09-11 14:27:24 +0000

    edited api

--- skillering/skillering-app/routes/api.php
+++ skillering/skillering-app/routes/api.php
@@ -1,14 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 
-// Auth routes (no CSRF protection needed here)
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
-
-// Task route
 Route::get('/tasks', [TaskController::class, 'index']);
