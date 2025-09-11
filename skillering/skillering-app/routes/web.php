de3effcae0a3be60ba483538abfb610434289ba9
Mariam Gad <mariamgad440@gmail.com>
2025-09-11 14:04:33 +0000

    modified web.php

--- skillering/skillering-app/routes/web.php
+++ skillering/skillering-app/routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController; 
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
-    return view('home'); 
+    return view('home');
 });
 
 Route::get('/tasks', [TaskController::class, 'index']);
+
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
